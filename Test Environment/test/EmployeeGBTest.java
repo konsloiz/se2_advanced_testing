cb2001790f5cbf3660d54547535c6a5556ebe69b
Konstantinos Loizas <konsloiz@gmail.com>
2021-05-10 17:23:37 +0200

    Final Version

--- Test Environment/test/EmployeeGBTest.java
+++ Test Environment/test/EmployeeGBTest.java
@@ -1,6 +1,5 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class EmployeeGBTest {
@@ -9,7 +8,7 @@ class EmployeeGBTest {
 	void hiringBonus() {
 		Employee e = new Employee(1993, 4000);
 		int actual = e.bonus(2020);
-		int expected = 8000;
+		int expected = 4000;
 		assertEquals(expected, actual);
 	}
 
